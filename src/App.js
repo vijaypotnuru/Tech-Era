2cec043cdd344e95cf4d166667801374a78fb1d9
vijaypotnuru <vijaypotnuru123@gmail.com>
2023-06-12 11:29:14 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {Switch, Route, Redirect} from 'react-router-dom'
 
 import Home from './components/Home'
+import NotFound from './components/NotFound'
+import CourseDetails from './components/CourseDetails'
 
 import './App.css'
 
@@ -8,6 +10,10 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/courses/:id" component={CourseDetails} />
+    <Route path="/not-found" component={NotFound} />
+
+    <Redirect to="not-found" />
   </Switch>
 )
 
